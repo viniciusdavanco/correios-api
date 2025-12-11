ffec9b16f4b3460fcf743336c99fff67dd9902a0
Vinícius Davanço <viniciusdavanco@outlook.com.br>
2025-12-11 08:43:24 -0300

    segundo commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'luannsr12/sdkcorreios' => array(
+            'pretty_version' => 'v1.5.2',
+            'version' => '1.5.2.0',
+            'reference' => '1b630e488b9b548025f71031a1497befdc14c070',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../luannsr12/sdkcorreios',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
